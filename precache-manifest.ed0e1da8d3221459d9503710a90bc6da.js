7830e458d9cbb47e18803300fdd9b0978cb1fa0b
Anton Barouka <52159126+blinkoliver@users.noreply.github.com>
2021-04-15 02:01:12 +0300

    Updates

---
+++ precache-manifest.ed0e1da8d3221459d9503710a90bc6da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dab12f68e5058ff3112e0a6c1bdc1bbf",
+    "url": "/coffee-counter-frontend/index.html"
+  },
+  {
+    "revision": "d52a77a6cc9a532c8263",
+    "url": "/coffee-counter-frontend/static/css/main.e7511cd1.chunk.css"
+  },
+  {
+    "revision": "42473c69709b1723f63c",
+    "url": "/coffee-counter-frontend/static/js/2.131e3388.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/coffee-counter-frontend/static/js/2.131e3388.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d52a77a6cc9a532c8263",
+    "url": "/coffee-counter-frontend/static/js/main.12643240.chunk.js"
+  },
+  {
+    "revision": "04512d317b2f4017d36d",
+    "url": "/coffee-counter-frontend/static/js/runtime-main.1ad39d16.js"
+  }
+]);
